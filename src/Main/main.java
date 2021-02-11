300abfeabc77453779537f84793f153e90ce9cfb
Guillermo Jabato <guillermo.jabato@ikasle.egibide.org>
2021-02-11 17:31:52 +0100

    Main

--- src/Main/main.java
+++ src/Main/main.java
@@ -3,9 +3,6 @@ package Main;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
 
 	}
 
